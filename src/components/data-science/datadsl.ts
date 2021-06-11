935f3745f2fd603fb31c74d469c7917b20b5e546
seankross <smk240@gmail.com>
2021-06-10 17:18:43 -0700

    added some blocks

--- src/components/data-science/datadsl.ts
+++ src/components/data-science/datadsl.ts
@@ -1,4 +1,8 @@
-import { BlockDefinition, CategoryDefinition } from "../blockly/toolbox"
+import { 
+    BlockDefinition, 
+    CategoryDefinition,
+    OptionsInputDefinition 
+} from "../blockly/toolbox"
 import BlockDomainSpecificLanguage from "../blockly/dsl/dsl"
 
 const colour = "#123456"
@@ -10,8 +14,8 @@ export class DataScienceBlockDomainSpecificLanguage
         const blocks: BlockDefinition[] = [
             {
                 kind: "block",
-                type: "ds_some_event",
-                message0: "some event",
+                type: "ds_tbl_mtcars",
+                message0: "mtcars",
                 args0: [],
                 inputsInline: true,
                 nextStatement: null,
@@ -19,9 +23,60 @@ export class DataScienceBlockDomainSpecificLanguage
             } as BlockDefinition,
             {
                 kind: "block",
-                type: "ds_some_block",
-                message0: "some block data",
+                type: "ds_tbl_trees",
+                message0: "trees",
                 args0: [],
+                inputsInline: true,
+                nextStatement: null,
+                colour,
+            } as BlockDefinition,
+            {
+                kind: "block",
+                type: "ds_tbl_col",
+                message0: "Column: %1",
+                args0: [
+                    <OptionsInputDefinition>{
+                        type: "field_dropdown",
+                        name: "COLUMN",
+                        options: [
+                            ["%{BKY_MATH_ADDITION_SYMBOL}", "ADD"],
+                            ["%{BKY_MATH_SUBTRACTION_SYMBOL}", "MINUS"],
+                            ["%{BKY_MATH_MULTIPLICATION_SYMBOL}", "MULTIPLY"],
+                            ["%{BKY_MATH_DIVISION_SYMBOL}", "DIVIDE"],
+                        ],
+                    },
+                ],
+                inputsInline: true,
+                output: "Number",
+                colour,
+            } as BlockDefinition,
+            {
+                kind: "block",
+                type: "ds_wr_arrange",
+                message0: "arrange",
+                args0: [],
+                inputsInline: true,
+                previousStatement: null,
+                nextStatement: null,
+                colour,
+            } as BlockDefinition,
+            {
+                kind: "block",
+                type: "ds_wr_filter",
+                message0: "filter",
+                args0: [],
+                inputsInline: true,
+                previousStatement: null,
+                nextStatement: null,
+                colour,
+            } as BlockDefinition,
+            {
+                kind: "block",
+                type: "ds_lg_compare",
+                message0: "Placeholder",
+                args0: [
+                   
+                ],
                 inputsInline: true,
                 output: "Number",
                 colour,
@@ -34,16 +89,42 @@ export class DataScienceBlockDomainSpecificLanguage
         return [
             <CategoryDefinition>{
                 kind: "category",
-                name: "Data Science",
+                name: "Datasets",
                 colour,
                 contents: [
                     {
                         kind: "block",
-                        type: "ds_some_event",
+                        type: "ds_tbl_mtcars",
                     },
                     {
                         kind: "block",
-                        type: "ds_some_block",
+                        type: "ds_tbl_trees",
+                    },
+                    {
+                        kind: "block",
+                        type: "ds_tbl_col",
+                    },
+                ],
+            },
+            <CategoryDefinition>{
+                kind: "category",
+                name: "Wrangle",
+                colour,
+                contents: [
+                    {
+                        kind: "block",
+                        type: "ds_wr_filter",
+                    },
+                ],
+            },
+            <CategoryDefinition>{
+                kind: "category",
+                name: "Logic",
+                colour,
+                contents: [
+                    {
+                        kind: "block",
+                        type: "ds_lg_compare",
                     },
                 ],
             },
